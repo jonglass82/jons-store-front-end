e36db126534e3aa2f8fe6d8fa4304c7961665b82
Jon Glass <jonglass82@gmail.com>
2019-10-14 17:17:56 -0500

    implement dashboard component

---
+++ src/components/dashboard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import axios from 'axios'
+
+
+class Dashboard extends React.Component {
+
+constructor(props){
+  super(props);
+  this.state = {
+
+  }
+}
+
+render (){
+  return <h1>This is the Dashboard!</h1>
+}
+
+
+}
+
+export default Dashboard
--- src/components/login.js
+++ src/components/login.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 class Login extends React.Component {
@@ -6,21 +7,57 @@ class Login extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-
+      message: "",
+      email: "", 
+      password: "", 
     }
   } 
 
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  submit () {
+      const params = {
+        email: this.email,
+        password: this.password
+      };
+
+      axios.post('http://localhost:3001/api/login', params).then(res => {
+        console.log(params);
+      // const email = res.data;
+      // const password = res.data;
+      // this.setState((state, props) => ({
+      //   email: email, 
+      //   password: password
+      // }));
+    })
+  };
+
+
   render () {
-    return <Form>
+    return <div className="container"> 
+
+    <h2>{this.state.email}</h2>
+    <h2>{this.state.password}</h2>
+
+      <h1>Login</h1>
+
+    <Form>
         <FormGroup>
           <Label for="exampleEmail">Email</Label>
-          <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+          <Input type="email" name="email" id="exampleEmail" value={this.email} onChange= {this.onChange} placeholder="" />
         </FormGroup>
         <FormGroup>
           <Label for="examplePassword">Password</Label>
-          <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+          <Input type="password" name="password" id="examplePassword" value={this.password} onChange= {this.onChange} placeholder="" />
         </FormGroup>
+        <Button>Submit</Button>
         </Form>
+
+        </div>
   }
 
 }
